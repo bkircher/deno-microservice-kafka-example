7bf71e2a7f65bdebbdca6b114dbf0dcf681b2b53
Ben Kircher <bkircher@0xadd.de>
2024-06-26 10:02:01 +0200

    VS Code: update tab size to 2 spaces everywhere

--- assert.ts
+++ assert.ts
@@ -1,16 +1,16 @@
 export { assert };
 
 const assert = <T>(
-    value: T | null | undefined,
-    errorMessage: string = "unexpectedly falsy, empty, null, or undefined",
+  value: T | null | undefined,
+  errorMessage: string = "unexpectedly falsy, empty, null, or undefined",
 ): NonNullable<T> => {
-    if (
-        value == null || // == also captures undefined
-        value === "" ||
-        !value ||
-        (typeof value === "number" && isNaN(value))
-    ) {
-        throw new Error(errorMessage);
-    }
-    return value;
+  if (
+    value == null || // == also captures undefined
+    value === "" ||
+    !value ||
+    (typeof value === "number" && isNaN(value))
+  ) {
+    throw new Error(errorMessage);
+  }
+  return value;
 };
